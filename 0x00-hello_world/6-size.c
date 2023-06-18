f02f11ca0ad9dd2fd40ada65a4c14cae90d538e0
Osunde Genuine <osundegenuine101@gmail.com>
2023-06-17 19:18:14 -0700

    6 size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#<stdio.h>
+/**
+*main-Entry point
+*
+*Return:Always 0(success) 
+*/
+int main(void)
+{
+	printf("Size of char:%d byte(s)\n",sizeof(char));
+	printf("Size of int:%d byte(s)\n",sizeof(int));
+	printf("Size of long int:%d bute(s)\n",sizeof(long int));
+	printf("Size of long long int:%d byte(s)\n"sizeof(long long int));
+	pritf("Size of float:%d byte(s)\n",sizeof(float));
+	return(0);
+}	
