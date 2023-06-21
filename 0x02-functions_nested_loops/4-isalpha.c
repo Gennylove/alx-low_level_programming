36cd7e8926af8c05a0f28a1be6c1a3ecd53f32d5
Osunde Genuine <osundegenuine101@gmail.com>
2023-06-20 22:10:45 -0700

    is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,7 +3,7 @@
 /**
  * _isalph - Checks alphabetic character
  * @c: The character to be checked
- * Return: 1 for alphabet character or 0 for anything else
+ * Return: 1 for alphabetic character or 0 for anything else
  */
 
 int _isalpha(int c)
