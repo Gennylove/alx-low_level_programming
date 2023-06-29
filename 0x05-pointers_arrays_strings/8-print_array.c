6ed4d1ba2b140a68560f7e4d7bfd14638dd65990
Osunde Genuine <osundegenuine101@gmail.com>
2023-06-29 02:07:56 -0700

    print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,7 +9,7 @@
 void print_array(int *a, int n)
 {
 int i;
-for (i = 0; i < (n - 1); i++
+for (i = 0; i < (n - 1); i++)
 {
 printf("%d, ", a[i]);
 }
@@ -20,4 +20,3 @@ printf("%d", a[n - 1]);
 }
 printf("\n");
 }
-
